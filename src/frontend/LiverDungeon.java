fcfb20455865a7bd0e2fc15f931b34e97e3244e0
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2015-11-23 18:28:24 -0600

    First commit;

---
+++ src/frontend/LiverDungeon.java
@@ -0,0 +1,12 @@
+package frontend;
+
+/**
+ * Created by ryan on 11/23/15.
+ */
+public class LiverDungeon
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello, world!");
+    }
+}
